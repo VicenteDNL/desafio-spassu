03d76f495e9fe3fd67bdece77561b0789b8e9f2d
Danilo <danilo.saraiva68@gmail.com>
2025-05-28 20:50:50 -0300

    feat(handling error): global errors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (Throwable $e, Request $request) {
+            return redirect()
+                ->back()
+                ->with('error', 'Ocorreu um erro inesperado, mas fique tranquilo que o problema já foi reportado a nossa equipe.');
+        });
     })->create();
